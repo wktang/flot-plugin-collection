6330b8fe0d23fddf6c0b4ad58e6aae9202dadb52
Ashish Bansal <bansal.ashish096@gmail.com>
2019-02-12 01:51:10 +0000

    Scale minX and minY too

--- src/flot/jquery.flot.autoscale.js
+++ src/flot/jquery.flot.autoscale.js
@@ -10,25 +10,29 @@
             var optsY = plot.getYAxes()[0].options;
             var optsX = plot.getXAxes()[0].options;
             var data = plot.getData();
-            var maxX = 0, maxY = 0;
+            var maxX = 0, maxY = 0, minX = 0, minY = 0;
 
             $.each(data, function (index, s) {
                 var max = autoScale(plot, s, s.data, s.datapoints);
                 maxX = Math.max(maxX, max['maxX']);
                 maxY = Math.max(maxY, max['maxY']);
+                minX = Math.max(minX, min['minX']);
+                minY = Math.max(minY, min['minY']);
             });
 
-            optsX.min = 0;
+            optsX.min = minX;
             optsX.max = maxX;
-            optsY.min = 0;
+            optsY.min = minY;
             optsY.max = maxY;
 
             plot.setupGrid();
             plot.draw();
 
             return {
-                min: optsY.min,
-                max: optsY.max
+                minY: optsY.min,
+                maxY: optsY.max,
+                minX: optsX.min,
+                maxX: optsX.max,
             };
         }
 
@@ -39,20 +43,31 @@
             if (series.lines.show || series.points.show || series.bars.show) {
                 var maxX = Number.NEGATIVE_INFINITY;
                 var maxY = Number.NEGATIVE_INFINITY;
+                var minX = Number.POSITIVE_INFINITY;
+                var minY = Number.POSITIVE_INFINITY;
 
                 for (var i = 0; i < data.length; i++) {
                     maxX = Math.max(maxX, data[i][0]);
                     maxY = Math.max(maxY, data[i][1]);
+                    minX = Math.min(minX, data[i][0]);
+                    minY = Math.min(minY, data[i][1]);
                 }
 
                 maxX += 2;
                 maxY += maxY * options.yaxis.autoscaleMargin * 10;
+
+                minY -= 2;
+                minY -= minY * options.yaxis.autoscaleMargin * 10;
                 return {
+                    minX: minX,
+                    minY: minY,
                     maxX: maxX,
                     maxY: maxY
                 };
             } else {
                 return {
+                    minX: 0,
+                    minY: 0,
                     maxX: 0,
                     maxY: 0
                 };
